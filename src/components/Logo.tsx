270af9c9138b6af15e5da55c5a6a5fbaf5e2eafd
Himanshu Shukla <hhimanshu030@gmail.com>
2026-09-23 10:23:19 +0100

    logo fixes

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo: React.FC<{ size?: number }> = ({ size = 40 }) => (
   <img
     src="/britinstitute_v1.png"
-    alt="EdTech Logo"
+    alt="Brit Institute Logo"
     width={size}
     height={size}
     style={{ borderRadius: 8 }}
